db8a34afb2ddb67559a1e3408a0621725c9a697c
A-Baji <bidabaji@gmail.com>
2023-07-02 22:04:19 -0500

    options and health bar ui

--- barrel-trainer/src/App.tsx
+++ barrel-trainer/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import { Checkbox, Radio, Space, Progress } from 'antd'
 import './App.css' // Import the CSS file with outline styles
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar'
 
 function App() {
     const [isHovered, setIsHovered] = useState(false)
@@ -13,25 +15,40 @@ function App() {
     }
     return (
         <div className='container'>
-            <div className='column'>
-                <button>Option 1</button>
-                <button>Option 2</button>
-                <button>Option 3</button>
+            <div className='column' id='left'>
+                <Space>
+                    <Checkbox>2 HP</Checkbox>
+                    <Checkbox>Auto Reset</Checkbox>
+                </Space>
             </div>
             <div className='image-container'>
-                <img
-                    src='/barrel.png'
-                    alt=''
-                    className={isHovered ? 'hovered-image' : ''}
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    draggable={false}
-                />
+                <Space className='healthbar' direction='vertical' align='center'>
+                    <Progress
+                        steps={3}
+                        percent={100}
+                        showInfo={false}
+                        strokeColor='#924243'
+                        status='normal'
+                    />
+                    <img
+                        src='/barrel.png'
+                        alt=''
+                        className={isHovered ? 'hovered-image' : ''}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        draggable={false}
+                        style={{
+                            cursor: 'pointer'
+                        }}
+                    />
+                </Space>
             </div>
-            <div className='column'>
-                <button>Option A</button>
-                <button>Option B</button>
-                <button>Option C</button>
+            <div className='column' id='right'>
+                <Radio.Group defaultValue='1' buttonStyle='solid'>
+                    <Radio value='1'>Level 1</Radio>
+                    <Radio value='7'>Level 7</Radio>
+                    <Radio value='13'>Level 13</Radio>
+                </Radio.Group>
             </div>
         </div>
     )
