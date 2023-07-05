65cf925c7af5520be341d0263413b57472ae3168
A-Baji <bidabaji@gmail.com>
2023-07-05 14:49:44 -0500

    precisiion timer

--- barrel-trainer/src/App.tsx
+++ barrel-trainer/src/App.tsx
@@ -1,24 +1,47 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Checkbox, Radio, Space, Progress } from 'antd'
-import './App.css' // Import the CSS file with outline styles
 import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
+import { useTimer } from 'react-use-precision-timer'
 import 'react-circular-progressbar/dist/styles.css'
+import './App.css' // Import the CSS file with outline styles
 
 function App() {
     const [isHovered, setIsHovered] = useState(false)
+    const [remainingTime, setRemainingTime] = useState(Number)
+
+    const callback = useCallback(() => {
+        timer.stop()
+        updateTimer.stop()
+    }, [])
+    const timer = useTimer({ delay: 2000 }, callback)
+    const update = useCallback(() => setRemainingTime(timer.getRemainingTime()), [])
+    const updateTimer = useTimer({ delay: 10 }, update)
 
     const handleMouseEnter = () => {
         setIsHovered(true)
     }
-
     const handleMouseLeave = () => {
         setIsHovered(false)
     }
+
+    useEffect(() => {
+        console.log(remainingTime)
+    }, [remainingTime])
+
     return (
         <div className='container'>
             <div className='column' id='left'>
                 <Space>
-                    <Checkbox>2 HP</Checkbox>
+                    <Checkbox
+                        onChange={(e) => {
+                            if (e.target.checked) {
+                                updateTimer.start()
+                                timer.start()
+                            }
+                        }}
+                    >
+                        2 HP
+                    </Checkbox>
                     <Checkbox>Auto Reset</Checkbox>
                 </Space>
             </div>
@@ -32,7 +55,7 @@ function App() {
                         status='normal'
                     />
                     <CircularProgressbarWithChildren
-                        value={75}
+                        value={(remainingTime / 2000) * 100}
                         strokeWidth={4}
                         styles={buildStyles({
                             rotation: 0.5,
